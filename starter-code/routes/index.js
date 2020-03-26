70bb98cf0bf003719750ac11e5374e287931bfda
Eduardo Álvarez <ceciliorubes@gmail.com>
2020-03-26 20:21:55 +0100

    Ejercico terminado con el error de la clave de maps corregido.

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -5,7 +5,8 @@ const Place = require("../models/place");
 router.get('/', (req, res, next) => {
   Place.find()
   .then((places) => {
-    res.render('index', { places });
+    let key = process.env.MAPS_KEY;
+    res.render('index', { places, key });
   })
 });
 
